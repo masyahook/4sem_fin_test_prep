6bed9eacb6aef4895768b7133c9bdbd57003b8a4
Andrew Sonin <sonin.cel@gmail.com>
2017-05-11 01:40:17 +0300

    Add files via upload

--- 10_max_pal.py
+++ 10_max_pal.py
@@ -8,9 +8,15 @@ def max_pal(seq):
         else:
             j = i
         cur = pals[j]
-        while i - cur >= 0 and i + cur < len_seq and seq[i - cur] == seq[i + cur]:
+        l_bound, r_bound = i - cur, i + cur
+        while l_bound >= 0 and r_bound < len_seq and seq[l_bound] == seq[r_bound]:
             cur += 1
+            l_bound -= 1
+            r_bound += 1
         pals[i] = cur
+        possible_rightest = cur - 1 + i
+        if possible_rightest > rightest:
+            rightest, pos = possible_rightest, i
     return [2 * x - 1 for x in pals]
 
 print(' '.join(map(str, max_pal(input()))))
